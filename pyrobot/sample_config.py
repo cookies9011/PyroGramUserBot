d6d42361500669818141bb016f4633583eb0d13a
cookies9011 <58487819+cookies9011@users.noreply.github.com>
2020-08-29 17:59:05 +0200

    Update sample_config.py

--- pyrobot/sample_config.py
+++ pyrobot/sample_config.py
@@ -3,14 +3,14 @@ import os
 class Config(object):
     LOGGER = True
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345)
-    API_HASH = os.environ.get("API_HASH", Nonr)
+    APP_ID = int(os.environ.get("APP_ID", 1045417)
+    API_HASH = os.environ.get("API_HASH", 103c70750fdb97721ff6523fed3c8ec0)
     # Get these values from my.telegram.org
     # string session for running on Heroku
     # some people upload their session files on GitHub or other third party hosting
     # websites, this might prevent the un-authorized use of the
     # confidential session files
-    HU_STRING_SESSION = os.environ.get("HU_STRING_SESSION", None)
+    HU_STRING_SESSION = os.environ.get("HU_STRING_SESSION", BACa_ApzkjiJj3eKEOzsTOoIFty2bRIfMyqxN0f8ynXJ29qR2GY24BfOORFynjThjAO5v_daEZu65ORI6W1Sd_BhH9NZuf4RtETn8eouYtD43wnK6mmCFtMkipkEwDHMFuk4xXSs855AUYIQSfTC29gJ1ystTK3DVbVYuic0nxgzahVmDRbTeNNkcy7D1uYu5myfgXWlndn8EbUxLfbrXReSEEMA2VUYaX1m_Sm4P4E2FRDRRdm6UKzCHK2VToy0WvVrpkGjETgPOJZpWSarDoAGM08FQMUp6wgw7-grRaN0I0YkmIfMM6X9ikj3VYzCPX0kMJNsBuZPiWPciRV_qbgYNr9yzAA)
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # specify command handler that should be used for the plugins
